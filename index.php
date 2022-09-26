dc5807c5f1ee16bff7bb297cc74cbb73b937818c
AllenPaul <allenofficial56@gmail.com>
2022-09-26 20:10:48 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Important Events of our batch">
+        <title>CS Rockers</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/x-icon" href="friends.ico">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=BhuTuka+Expanded+One&family=Dancing+Script&family=Great+Vibes&family=Open+Sans:wght@500&family=Oswald&display=swap" rel="stylesheet">
+    </head>
+        <body>
+            <div class="main">
+                <div class="navbar">
+                    <h2 class="logo">T I N Y C O D E R S</h2>
+                    <h4 class="logo2">Since2020</h4>
+                 </div>
+        
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="aboutdept.html">ABOUTOURDEPARTMENT</a></li>
+                        <li><a href="#">IMPORTANTEVENTS</a></li>
+                           
+                    </ul>
+                </div>
+                <div class="content">
+                    <h1>Let&nbsp;Us&nbsp;Relish&nbsp;Cherish &nbsp;&&nbsp;Reminisce&nbsp;our&nbsp;Memories</h1>
+                    <p class="par">
+                        "Hard to forget such strong memories with the &nbsp;<a href ="https://fonts.google.com/"><input type="submit" value="Best People"></a> <br> 
+                         still reliving this in my biggest dreams
+                         so glad<br> we got pictures or else it didn't happen"........
+                     </p>
+                </div>       
+                <div class="image">
+                    <img class="image__img" src="assets/preview1.jpeg" alt="College">
+                    <div class="image__overlay image__overlay--blur">
+                        <div class="image__title">Maps</div> 
+                    </div>
+                </div>
+                <div class="image1">
+                    <img class="image__img1" src="assets/preview.jpeg" alt="College">
+                    <div class="image__overlay1 image__overlay--blur1">
+                        <div class="image__title1">Maps</div> 
+                    </div>
+                </div>
+
+                   
+                 <div class="form">
+                    <h2>Login Here</h2>
+                    <input type="email" name="email" placeholder="Enter Email Here">
+                    <input type="password" name="" placeholder="Enter Password Here">
+                    <button class="btnn"><a href="#">Login</a></button>
+                    <p class="link">Don't have an account<br>
+                    <a href="#">Sign up </a> here</a></p>
+                </div>         
+            </div>   
+             
+        </body>
+    
+</html>
